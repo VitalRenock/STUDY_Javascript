cc5c6aa03e424be1b9b60067306033dceb7594c4
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-12-03 03:05:31 +0100

    refacto

--- index.php
+++ index.php
@@ -4,14 +4,42 @@
 
     <?php include("includes/meta&css.php"); ?>
     <?php //include("includes/connect_db.php"); ?>
-
+    
     <title>Accueil</title>
+
 </head>
 <body>
-
+    
     <?php include("includes/header.php"); ?>
     <?php include("includes/menu.php"); ?>
 
+    <article>
+        <pre>
+            <code class="html">
+                <p id="monArticle">Article1</p>
+            </code>
+        </pre>
+    </article>
+
+
+    <script type="text/javascript" src='/javascript/custom_methode.js'></script>
+
+    <script>
+        var testStr = 
+        "<!DOCTYPE html>\r\
+        <html lang=\"en\">\r\
+        <head>\r\
+            <meta charset=\"UTF-8\">\r\
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\
+            <title>Document</title>\r\
+        </head>\r\
+        <body>\r\
+            \r\
+        </body>\r\
+        </html>";
+        
+        ReplaceInnerHTMLbyID('monArticle', ConvertSTRINGtoHTML(testStr));
+    </script>
     
 <!-- ARCHIVES -->
     
@@ -25,13 +53,6 @@
         // }
         // $columnsToString .= ") ";
     ?>
-
-
-    <!-- Modifier du CSS avec javascript -->
-    <!-- <script type="text/javascript">
-        // var css_value = "<?php //echo $value; ?>"                // Passage d'une variable PHP vers JS
-        // document.body.style.backgroundColor = css_value          // Changement du css
-    </script> -->
-
+    
 </body>
 </html>
